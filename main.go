031377e89ff0a692cc06de6956cc139be19d3ded
Dan Katz <dgkatz@gmail.com>
2020-07-10 17:18:29 -0400

    hello world endpoint

--- main.go
+++ main.go
@@ -1,1 +1,25 @@
-package learn_go_api
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	_, err := fmt.Fprintf(w, "Welcome to the HomePage!")
+	if err != nil{
+		fmt.Println("Failed to write response")
+	} else {
+		fmt.Println("hello from helloWorld endpoint!")
+	}
+}
+
+func registerHandlers() {
+	http.HandleFunc("/hello", helloWorld)
+	log.Fatal(http.ListenAndServe("0.0.0.0:8001", nil))
+}
+
+func main() {
+	registerHandlers()
+}
